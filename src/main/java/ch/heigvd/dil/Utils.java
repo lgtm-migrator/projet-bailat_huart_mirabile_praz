2c1c0d5f54d706e0951a8ea11bd450574b74973b
Hugo Huart <hugo.huart@heig-vd.ch>
2022-03-29 11:19:29 +0200

    [DONE] Cosmetic:Better method order

--- src/main/java/ch/heigvd/dil/Utils.java
+++ src/main/java/ch/heigvd/dil/Utils.java
@@ -5,12 +5,12 @@ public class Utils {
     private static final String VERSION = "0.0.1-pre_alpha";
     private static final String COPYRIGHT_TEXT = "Copyright © 2022 Bailat, Huart, Mirabile, Praz";
 
-    public static String getVersion() {
-        return VERSION;
-    }
-
     public static String getExecutableName() {
         return EXECUTABLE_NAME;
+    }
+
+    public static String getVersion() {
+        return VERSION;
     }
 
     public static String getFullName() {
