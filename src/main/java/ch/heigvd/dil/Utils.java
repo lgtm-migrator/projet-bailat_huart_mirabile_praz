c413bcce6677145588541713603331d63ac9f9e0
Théo Mirabile <theo.mirabile@me.com>
2022-04-06 11:12:17 +0200

    [done] edited writeYamlFile function to ignore the filePath property when parsing the page metadata, and added appendToFile function

--- src/main/java/ch/heigvd/dil/Utils.java
+++ src/main/java/ch/heigvd/dil/Utils.java
@@ -2,12 +2,13 @@ package ch.heigvd.dil;
 
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.introspector.Property;
 import org.yaml.snakeyaml.nodes.Tag;
 import org.yaml.snakeyaml.representer.Representer;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Utils {
 
@@ -18,11 +19,24 @@ public class Utils {
         options.setCanonical(false);
         options.setDefaultScalarStyle(DumperOptions.ScalarStyle.PLAIN);
 
-        Representer representer = new Representer();
+        Representer representer = new Representer() {
+            @Override
+            protected Set<Property> getProperties(Class<?> type) {
+                Set<Property> properties = super.getProperties(type);
+                return properties.stream().filter(v -> !v.getName().equals("filePath")).collect(Collectors.toSet());
+            }
+        };
         representer.addClassTag(o.getClass(), Tag.MAP);
 
         Yaml yaml = new Yaml(representer, options);
         PrintWriter writer = new PrintWriter(filePath);
         yaml.dump(o, writer);
+    }
+
+    public static void appendToFile(String s, File filePath) throws IOException {
+        FileWriter fw = new FileWriter(filePath, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(s);
+        bw.close();
     }
 }
