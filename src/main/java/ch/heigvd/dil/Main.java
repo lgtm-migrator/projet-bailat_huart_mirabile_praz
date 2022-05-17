a1b2d160bb08cc0b59b9802e55dcea0feb1514f1
PrazTobie <tobie.praz@heig-vd.ch>
2022-05-17 14:52:09 +0200

    [FIX] Formatting

--- src/main/java/ch/heigvd/dil/Main.java
+++ src/main/java/ch/heigvd/dil/Main.java
@@ -7,27 +7,21 @@ import ch.heigvd.dil.commands.Version;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 
-@CommandLine.Command(name = "statique",
-subcommands = {
-        Init.class,
-        Build.class,
-        Clean.class,
-        Serve.class,
-        Version.class
-})
-
+@CommandLine.Command(
+    name = "statique",
+    subcommands = {Init.class, Build.class, Clean.class, Serve.class, Version.class})
 public class Main implements Callable<Integer> {
 
-    public static void main(String[] args) {
-        int exitCode = new CommandLine(new Main()).execute(args);
-        if (exitCode != 0) {
-            System.exit(exitCode);
-        }
+  public static void main(String[] args) {
+    int exitCode = new CommandLine(new Main()).execute(args);
+    if (exitCode != 0) {
+      System.exit(exitCode);
     }
+  }
 
-    @Override
-    public Integer call() {
-        System.out.println("Unknown command, try 'statique build|clean|init|serve|-version'");
-        return 0;
-    }
+  @Override
+  public Integer call() {
+    System.out.println("Unknown command, try 'statique build|clean|init|serve|-version'");
+    return 0;
+  }
 }
