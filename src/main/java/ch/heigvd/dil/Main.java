8dfe1dc270ae71464478a1a34f2fda4977874796
PrazTobie <tobie.praz@heig-vd.ch>
2022-05-25 11:04:07 +0200

    [FIX] Get version from pom.xml, display usage text by default

--- src/main/java/ch/heigvd/dil/Main.java
+++ src/main/java/ch/heigvd/dil/Main.java
@@ -1,13 +1,22 @@
 package ch.heigvd.dil;
 
-import ch.heigvd.dil.commands.*;
+import ch.heigvd.dil.commands.Build;
+import ch.heigvd.dil.commands.Clean;
+import ch.heigvd.dil.commands.Init;
+import ch.heigvd.dil.commands.Serve;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 
 @CommandLine.Command(
     name = "statique",
-    subcommands = {Init.class, Build.class, Clean.class, Serve.class, Version.class})
+    subcommands = {Init.class, Build.class, Clean.class, Serve.class},
+    versionProvider = Main.ManifestVersionProvider.class)
 public class Main implements Callable<Integer> {
+  @CommandLine.Option(
+      names = {"-v", "--version"},
+      versionHelp = true,
+      description = "Print version info")
+  boolean versionRequested;
 
   public static void main(String[] args) {
     int exitCode = new CommandLine(new Main()).execute(args);
@@ -18,7 +27,13 @@ public class Main implements Callable<Integer> {
 
   @Override
   public Integer call() {
-    System.out.println("Unknown command, try 'statique build|clean|init|serve|-version'");
+    CommandLine.usage(this, System.out);
     return 0;
+  }
+
+  static class ManifestVersionProvider implements CommandLine.IVersionProvider {
+    public String[] getVersion() {
+      return new String[] {getClass().getPackage().getImplementationVersion()};
+    }
   }
 }
