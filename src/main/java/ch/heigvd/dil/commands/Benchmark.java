efb83ff72c59b508b4aae2334267c084f0a4773b
Hugo Huart <hugo.huart@heig-vd.ch>
2022-06-01 17:36:08 +0200

    [WIP] Benchmark command somewhat working

--- src/main/java/ch/heigvd/dil/commands/Benchmark.java
+++ src/main/java/ch/heigvd/dil/commands/Benchmark.java
@@ -1,10 +1,11 @@
 package ch.heigvd.dil.commands;
 
-import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.openjdk.jmh.Main;
 import org.openjdk.jmh.annotations.*;
 import picocli.CommandLine;
 
+import java.io.File;
+import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.util.concurrent.Callable;
 
@@ -17,19 +18,31 @@ public class Benchmark implements Callable<Integer> {
   @CommandLine.Parameters(index = "0", paramLabel = "PATH", description = "Path to site folder")
   private Path root;
 
+  static final String JAR_PATH;
+
+  static {
+    try {
+      JAR_PATH =
+          new File(Benchmark.class.getProtectionDomain().getCodeSource().getLocation().toURI())
+              .getPath();
+    } catch (URISyntaxException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
   @org.openjdk.jmh.annotations.Benchmark
   @Fork(value = 1, warmups = 1)
   @Warmup(iterations = 2, time = 2)
   @Measurement(time = 2)
   @BenchmarkMode(Mode.AverageTime)
-  public void evaluateBuild() {}
+  public void buildBenchmark() throws Exception {
+    Runtime.getRuntime().exec(String.format("java -jar %s build %s", JAR_PATH, root));
+  }
 
-  public Integer call() {
-    try {
-      Main.main(new String[] {""});
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-    }
+  public Integer call() throws Exception {
+    Runtime.getRuntime().exec(String.format("java -jar %s init %s", JAR_PATH, root));
+    Main.main(new String[] {""});
+
     return 0;
   }
 }
