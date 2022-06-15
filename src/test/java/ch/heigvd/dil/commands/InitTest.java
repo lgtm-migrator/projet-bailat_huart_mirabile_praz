45a1816cdfebdb500eed99d8d0422b88170e629d
PrazTobie <tobie.praz@heig-vd.ch>
2022-06-15 16:11:11 +0200

    [FIX] Init tests

--- src/test/java/ch/heigvd/dil/commands/InitTest.java
+++ src/test/java/ch/heigvd/dil/commands/InitTest.java
@@ -1,74 +1,69 @@
 package ch.heigvd.dil.commands;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.Test;
-import picocli.CommandLine;
+import static org.junit.Assert.*;
 
+import ch.heigvd.dil.Utils;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.apache.commons.io.FileUtils.deleteDirectory;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Test;
+import picocli.CommandLine;
 
 public class InitTest {
+  static final Path TEST_FOLDER = Path.of("test");
 
+  @Test
+  public void shouldReturnErrorIfPathAlreadyExist() throws IOException {
+    System.out.println("-- PATH ALREADY EXISTS");
 
-    static final String PATHDIR = "./testSite";
+    // create new dir + add files
+    TEST_FOLDER.toFile().mkdirs();
+    new PrintWriter(TEST_FOLDER.resolve("rndFile.txt").toString(), StandardCharsets.UTF_8);
+    int result = new CommandLine(new Init()).execute(TEST_FOLDER.toString());
 
-    @Test
-    public void shouldReturnErrorIfPathAlreadyExist() throws IOException {
-        System.out.println("-- PATH ALREADY EXISTS");
+    assertEquals(1, result);
+  }
 
-        //create new dir + add files
-        new File(PATHDIR).mkdirs();
-        new PrintWriter(PATHDIR + "/rndFile.txt", "UTF-8");
-        int result = new CommandLine(new Init()).execute(PATHDIR);
+  @Test
+  public void shouldContainsDefaultFiles() {
 
-        //delete folder and files
-        deleteDirectory(new File(PATHDIR));
+    System.out.println("-- CONTAINS DEFAULT FILES");
 
-        assertEquals(1, result);
+    // define which files should exist
+    List<String> defaultfiles = Arrays.asList("config.yaml", "index.md");
+
+    new CommandLine(new Init()).execute(TEST_FOLDER.toString());
+    List files = List.of(TEST_FOLDER.toFile().list());
+
+    assertTrue(files.containsAll(defaultfiles));
+  }
+
+  @Test
+  public void configShouldNotBeEmpty() {
+
+    System.out.println("-- CONFIG FILE NOT EMPTY");
+
+    new CommandLine(new Init()).execute(TEST_FOLDER.toString());
+
+    File configFile = TEST_FOLDER.resolve(Utils.Paths.CONFIG_FILENAME).toFile();
+
+    assertNotEquals(0, configFile.length());
+  }
+
+  @After
+  public void clean() {
+    System.out.println("--- CLEANING ---");
+
+    try {
+      FileUtils.cleanDirectory(TEST_FOLDER.toFile());
+    } catch (Exception e) {
+      System.out.println(e);
     }
-
-    @Test
-    public void shouldContainsDefaultFiles() throws IOException {
-
-        System.out.println("-- CONTAINS DEFAULT FILES");
-
-        //define which files should exist
-        List<String> defaultfiles = Arrays.asList("config.yaml", "index.md");
-        Path dirPath = Path.of(PATHDIR);
-
-        new CommandLine(new Init()).execute(dirPath.toString());
-        List files = List.of(dirPath.toFile().list());
-
-        //delete folder and files
-        deleteDirectory(new File(dirPath.toString()));
-
-        assertEquals(true, files.containsAll(defaultfiles));
-
-    }
-
-    @Test
-    public void configShouldNotBeEmpty() throws IOException {
-
-        System.out.println("-- CONFIG FILE NOT EMPTY");
-
-        Path dirPath = Path.of(PATHDIR);
-        new CommandLine(new Init()).execute(dirPath.toString());
-
-        File configFile = new File(PATHDIR + "/config.yaml");
-
-        //delete folder and files
-        deleteDirectory(new File(PATHDIR));
-
-        assertNotEquals(0, configFile.length());
-
-    }
-
+  }
 }
