90c585b4e9c7d1011ff1f5ddc863ff98d00a7db6
Théo Mirabile <theo.mirabile@me.com>
2022-06-15 13:29:25 +0200

    [WIP] Base structure for tests

---
+++ src/test/java/ch/heigvd/dil/commands/BuildTest.java
@@ -0,0 +1,14 @@
+package ch.heigvd.dil.commands;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/** Unit test for simple App. */
+public class BuildTest {
+  /** Rigorous Test :-) */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
